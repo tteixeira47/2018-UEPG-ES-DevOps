f80e9359313027995113e619120c307ddf8b2a1c
Luiz Pedro Petroski <petroski@ead.uepg.br>
2018-12-05 17:59:01 -0200

    Adicionando fibonacci

--- src/test/java/TesteFibo.java
+++ src/test/java/TesteFibo.java
@@ -7,6 +7,22 @@ public class TesteFibo {
     @Test
     public void Teste1() {
         Fibonacci f = new Fibonacci();
-        assertEquals(f.fibo(1),1L, "O Fibonacci 1 deve ser 1");
+        assertEquals(f.fibo(1),1L);
+    }
+    public void Teste2() {
+        Fibonacci f = new Fibonacci();
+        assertEquals(f.fibo(1),2L);
+    }
+    public void Teste3() {
+        Fibonacci f = new Fibonacci();
+        assertEquals(f.fibo(9),34L);
+    }
+    public void Teste4() {
+        Fibonacci f = new Fibonacci();
+        assertEquals(f.fibo(13),233L);
+    }
+    public void Teste5() {
+        Fibonacci f = new Fibonacci();
+        assertEquals(f.fibo(29),514229L);
     }
 }
