988c2fddaeb5f898b7355b46aa53621de9d0fed5
Luiz Pedro Petroski <petroski@ead.uepg.br>
2018-12-05 18:10:04 -0200

    Adicionando fibonacci

--- src/test/java/TesteFibo.java
+++ src/test/java/TesteFibo.java
@@ -7,26 +7,26 @@ public class TesteFibo {
     @Test
     public void Teste1() {
         Fibonacci f = new Fibonacci();
-        assertEquals(f.fibo(1),1L);
+        assertEquals(1L, f.fibo(1));
     }
     @Test
     public void Teste2() {
         Fibonacci f = new Fibonacci();
-        assertEquals(f.fibo(1),2L);
+        assertEquals(2L, f.fibo(3));
     }
     @Test
     public void Teste3() {
         Fibonacci f = new Fibonacci();
-        assertEquals(f.fibo(9),34L);
+        assertEquals(34L, f.fibo(9));
     }
     @Test
     public void Teste4() {
         Fibonacci f = new Fibonacci();
-        assertEquals(f.fibo(13),233L);
+        assertEquals(233L, f.fibo(13));
     }
     @Test
     public void Teste5() {
         Fibonacci f = new Fibonacci();
-        assertEquals(f.fibo(29),514229L);
+        assertEquals(514229L, f.fibo(29));
     }
 }
