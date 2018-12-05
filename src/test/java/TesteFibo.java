7357bc5a433ec2ce0a18c0e62866361363fc4f93
Luiz Pedro Petroski <petroski@ead.uepg.br>
2018-12-05 17:50:25 -0200

    Adicionando fibonacci

--- src/test/java/TesteFibo.java
+++ src/test/java/TesteFibo.java
@@ -7,6 +7,6 @@ public class TesteFibo {
     @Test
     public void Teste1() {
         Fibonacci f = new Fibonacci();
-        assertEquals(f.fibo(1), 1, "O 1 numero de fibonacci deve ser 1");
+        assertEquals(f.fibo(1),1L);
     }
 }
