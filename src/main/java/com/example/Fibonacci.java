9e9642b10e3721b7a0d7537ee5e522353dcac837
Luiz Pedro Petroski <petroski@ead.uepg.br>
2018-12-05 17:39:59 -0200

    Adicionando fibonacci

--- src/main/java/com/example/Fibonacci.java
+++ src/main/java/com/example/Fibonacci.java
@@ -5,7 +5,7 @@ public class Fibonacci{
     public Fibonacci(){
 
     }
-    static long fibo(int n) {
+    public static long fibo(int n) {
         if (n < 2) {
             return n;
         } else {
