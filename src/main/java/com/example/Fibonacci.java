2738564cf7d5a73bbf73214c5274dad427d93109
Thiago Teixeira <thiagoh.teixeira@outlook.com>
2018-12-06 02:26:07 -0200

    Revert "fibo.1"
    This reverts commit a32690e7f3fb6dd2a66d59b4d70d38cb44dccc8c.

--- src/main/java/com/example/Fibonacci.java
+++ src/main/java/com/example/Fibonacci.java
@@ -7,7 +7,7 @@ public class Fibonacci{
     }
     public static long fibo(int n) {
         if (n < 2) {
-            return n*2;
+            return n;
         } else {
             return fibo(n - 1) + fibo(n - 2);
         }
