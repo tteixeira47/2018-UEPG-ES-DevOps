d7f36c2c5632b546d554d53d9bcc0d833eaf3b27
Thiago Teixeira <thiagoh.teixeira@outlook.com>
2018-12-06 03:16:44 -0200

    corrigindo precisão dos valores

--- src/test/java/TesteFibo.java
+++ src/test/java/TesteFibo.java
@@ -7,26 +7,26 @@ public class TesteFibo {
     @Test
     public void Teste1() {
         Fibonacci f = new Fibonacci();
-        assertEquals(2D, f.fibo(1));
+        assertEquals(2D, f.fibo(1), 0.01);
     }
     @Test
     public void Teste2() {
         Fibonacci f = new Fibonacci();
-        assertEquals(8D, f.fibo(3));
+        assertEquals(8D, f.fibo(3), 0.01);
     }
     @Test
     public void Teste3() {
         Fibonacci f = new Fibonacci();
-        assertEquals(512D, f.fibo(9));
+        assertEquals(512D, f.fibo(9), 0.01);
     }
     @Test
     public void Teste4() {
         Fibonacci f = new Fibonacci();
-        assertEquals(4D, f.fibo(2));
+        assertEquals(4D, f.fibo(2), 0.01);
     }
     @Test
     public void Teste5() {
         Fibonacci f = new Fibonacci();
-        assertEquals(16D, f.fibo(4));
+        assertEquals(16D, f.fibo(4), 0.01);
     }
 }
