7d1e185a9541d243cfeed677217080f6fe7f00ce
Thiago Teixeira <thiagoh.teixeira@outlook.com>
2018-12-06 03:11:44 -0200

    correção do teste double

--- src/test/java/TesteFibo.java
+++ src/test/java/TesteFibo.java
@@ -7,26 +7,26 @@ public class TesteFibo {
     @Test
     public void Teste1() {
         Fibonacci f = new Fibonacci();
-        assertEquals(2, f.fibo(1));
+        assertEquals(2D, f.fibo(1));
     }
     @Test
     public void Teste2() {
         Fibonacci f = new Fibonacci();
-        assertEquals(8, f.fibo(3));
+        assertEquals(8D, f.fibo(3));
     }
     @Test
     public void Teste3() {
         Fibonacci f = new Fibonacci();
-        assertEquals(512, f.fibo(9));
+        assertEquals(512D, f.fibo(9));
     }
     @Test
     public void Teste4() {
         Fibonacci f = new Fibonacci();
-        assertEquals(4, f.fibo(2));
+        assertEquals(4D, f.fibo(2));
     }
     @Test
     public void Teste5() {
         Fibonacci f = new Fibonacci();
-        assertEquals(16, f.fibo(4));
+        assertEquals(16D, f.fibo(4));
     }
 }
